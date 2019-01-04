0bd58fa0a354df1e4bdc1f99b4c9c0c402705812
serhii.zaidlin <serhii.zaidlin@betlab.com>
2019-01-04 10:31:18 +0200

    commit

---
+++ lib/logstash/filters/date_to_local.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+require "date"
+
+class LogStash::Filters::DateToLocal < LogStash::Filters::Base
+
+  config_name "date_to_local"
+
+
+  config :source, :validate => :string, :default => LogStash::Event::TIMESTAMP
+  config :target, :validate => :string, :default => "localdate"
+
+
+  public
+  def register
+
+  end # def register
+
+  public
+  def filter(event)
+
+    if @source
+      dt = DateTime.new(@source)
+	  local_time = dt.new_offset(DateTime.now.offset)
+      event.set(@target,local_time.strftime('%d %b %H:%M:%S.%L %z'))
+      @logger.debug? && @logger.debug("Message is now: #{event.get("message")}")
+    end
+
+
+    filter_matched(event)
+  end # def filter
+end # class LogStash::Filters::Example
+
+
+
+
