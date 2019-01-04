57b100926525f10c904474429e8a34d3899f872c
user419e <42600665+user419e@users.noreply.github.com>
2019-01-04 15:38:43 +0200

    Update date_to_local.rb

--- lib/logstash/filters/date_to_local.rb
+++ lib/logstash/filters/date_to_local.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 require "logstash/filters/base"
-require "logstash/namespace"
+#require "logstash/namespace"
 require "date"
 
 class LogStash::Filters::DateToLocal < LogStash::Filters::Base
